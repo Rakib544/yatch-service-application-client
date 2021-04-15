f936e8300a14a1f8a5cfbcb1fc27962c7b5ae1af
Rakib544 <md.rakib10122003@gmail.com>
2021-04-15 11:47:01 +0600

    working on my navbar

---
+++ src/components/Home/Header/Navbar/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        cName: ''
+    },
+    {
+        title: 'About Us',
+        cName: ''
+    },
+    {
+        title: 'Contact',
+        cName: ''
+    },
+    {
+        title: 'Admin',
+        cName: ''
+    },
+    {
+        title: 'Login',
+        cName: ''
+    },
+]
--- src/components/Home/Header/Navbar/Navbar.js
+++ src/components/Home/Header/Navbar/Navbar.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { MenuItems } from './MenuItems';
 
 const Navbar = () => {
     return (
-        <div>
-            
-        </div>
+        <nav className="navbarItems">
+            <h1 className="navbar-logo">
+                logo
+            </h1>
+            <div className="menu-icon">Mobile menu</div>
+            <ul>
+                {
+                    MenuItems.map((item, index) => {
+                        return(
+                            <li key={index}>
+                                <a className={item.cName}>{item.title}</a>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </nav>
     );
 };
 
